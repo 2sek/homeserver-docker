8abd49d19d8cf31cbf6edc0c14ce6ac881d03bf8
2sek <2sekproductions@gmail.com>
2023-07-03 19:57:16 +0200

    Fix restart script

--- restart.sh
+++ restart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Restarts and updates containers.
 
-for dir in $(find . -mindepth 1 -maxdepth 1 -type d -not -path '.*');
+for dir in $(find . -mindepth 1 -maxdepth 1 -type d -not -path './.*')
 do
   if [[ -n docker-compose.yaml ]]; then
     cd $dir
